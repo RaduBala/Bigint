59188794eba145bb7e8dddc28a0d9e87b948332c
RaduBala <63859371+RaduBala@users.noreply.github.com>
2020-04-29 20:31:50 +0300

    Create Bigint_cfg.h

---
+++ Bigint/Bigint_cfg.h
@@ -0,0 +1,19 @@
+/*
+* Bigint_cfg.h
+*
+*  Created on: Mar 6, 2019
+*      Author: B Radu
+*/
+
+#ifndef _BIGINT_CFG_H_
+#define _BIGINT_CFG_H_
+
+
+/*****************************************************************************/
+/*
+** \brief  The maximum length in data units a big integer can have.
+*/
+#define  BIGINT_MAX_SIZE    128
+
+
+#endif /* _BIGINT_CFG_H_ */
